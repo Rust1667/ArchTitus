562e7994f81271e3400d46cdb29c1d8a904e81b2
Rust1667 <topphotoblog_byddwn@aleeas.com>
2024-03-28 19:44:52 +0100

    Update preinstall_for_vm.sh

--- scripts/preinstall_for_vm.sh
+++ scripts/preinstall_for_vm.sh
@@ -11,34 +11,46 @@ sgdisk -Z /dev/vda # zap all on disk
 #sgdisk -a 2048 -o /dev/vda # new gpt disk 2048 alignment
 
 # make partitions
-sgdisk -n 1::+300M --typecode=1:ef02 /dev/vda
-sgdisk -n 2::+8G --typecode=2:8300 /dev/vda
+# sgdisk -n 1::+300M --typecode=1:ef02 /dev/vda
+# sgdisk -n 2::+8G --typecode=2:8300 /dev/vda
+# sgdisk -n 3::+4G --typecode=3:8300 /dev/vda
+# sgdisk -n 4::-0 --typecode=4:8300 /dev/vda
 
-# check if /dev/vda2 exists
-if [ ! -b /dev/vda2 ]; then
-    echo "Error: /dev/vda2 does not exist"
-    exit 1
+# # create partitions
+sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' /dev/vda # partition 1 (BIOS Boot Partition)
+sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' /dev/vda # partition 2 (UEFI Boot Partition)
+sgdisk -n 3::-8G --typecode=3:8300 --change-name=3:'ROOT' /dev/vda # partition 3 (Root), default start, remaining
+sgdisk -n 3::+4G --typecode=3:8300 --change-name=4:'HOME' /dev/vda # partition 4
+sgdisk -n 4::-0 --typecode=4:8300 --change-name=5:'STORAGE' /dev/vda # partition 5
+if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
+    sgdisk -A 1:set:2 /dev/vda
 fi
+partprobe /dev/vda # reread partition table to ensure it is correct
 
-sgdisk -n 3::+4G --typecode=3:8300 /dev/vda
-sgdisk -n 4::-0 --typecode=4:8300 /dev/vda
+
+
+# check if /dev/vda3 exists
+# if [ ! -b /dev/vda3 ]; then
+#     echo "Error: /dev/vda3 does not exist"
+#     exit 1
+# fi
+
+# check if vda3 is correctly created and a btrfs system
+# if [ ! -b /dev/vda3 -o "$(blkid -s TYPE -o value /dev/vda2)" != "btrfs" ]; then
+#     echo "Error: /dev/vda3 is not a btrfs system"
+#     exit 1
+# fi
 
 # format partitions
 mkfs.fat -F32 /dev/vda1
-mkfs.btrfs /dev/vda2 -f
-
-# check if vda2 is correctly created and a btrfs system
-if [ ! -b /dev/vda2 -o "$(blkid -s TYPE -o value /dev/vda2)" != "btrfs" ]; then
-    echo "Error: /dev/vda2 is not a btrfs system"
-    exit 1
-fi
-
-mkfs.ext4 /dev/vda3
+mkfs.fat -F32 /dev/vda2
+mkfs.btrfs /dev/vda3 -f
 mkfs.ext4 /dev/vda4
+mkfs.ext4 /dev/vda5
 
 # make btrfs subvolumes
 mkdir /mnt
-mount /dev/vda2 /mnt
+mount /dev/vda3 /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@var
 btrfs subvolume create /mnt/@tmp
@@ -46,11 +58,11 @@ btrfs subvolume create /mnt/@.snapshots
 umount /mnt
 
 # mount btrfs subvolumes
-mount -o noatime,compress=zstd,subvol=@ /dev/vda2 /mnt
+mount -o noatime,compress=zstd,subvol=@ /dev/vda3 /mnt
 mkdir -p /mnt/{var,tmp,.snapshots}
-mount -o noatime,compress=zstd,subvol=@var /dev/vda2 /mnt/var
-mount -o noatime,compress=zstd,subvol=@tmp /dev/vda2 /mnt/tmp
-mount -o noatime,compress=zstd,subvol=@.snapshots /dev/vda2 /mnt/.snapshots
+mount -o noatime,compress=zstd,subvol=@var /dev/vda3 /mnt/var
+mount -o noatime,compress=zstd,subvol=@tmp /dev/vda3 /mnt/tmp
+mount -o noatime,compress=zstd,subvol=@.snapshots /dev/vda3 /mnt/.snapshots
 
 # mount home
 mkdir /mnt/home
