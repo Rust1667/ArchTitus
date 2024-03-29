495006b624528dffbb5bd4a06dbe521e7d2f8ac4
Rust1667 <topphotoblog_byddwn@aleeas.com>
2024-03-29 15:56:10 +0100

    disable snapper and plymouth

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -97,41 +97,41 @@ echo "  NetworkManager enabled"
 systemctl enable avahi-daemon.service
 echo "  Avahi enabled"
 
-if [[ "${FS}" == "luks" || "${FS}" == "btrfs" ]]; then
-echo -ne "
--------------------------------------------------------------------------
-                    Creating Snapper Config
--------------------------------------------------------------------------
-"
+# if [[ "${FS}" == "luks" || "${FS}" == "btrfs" ]]; then
+# echo -ne "
+# -------------------------------------------------------------------------
+#                     Creating Snapper Config
+# -------------------------------------------------------------------------
+# "
 
-SNAPPER_CONF="$HOME/ArchTitus/configs/etc/snapper/configs/root"
-mkdir -p /etc/snapper/configs/
-cp -rfv ${SNAPPER_CONF} /etc/snapper/configs/
+# SNAPPER_CONF="$HOME/ArchTitus/configs/etc/snapper/configs/root"
+# mkdir -p /etc/snapper/configs/
+# cp -rfv ${SNAPPER_CONF} /etc/snapper/configs/
 
-SNAPPER_CONF_D="$HOME/ArchTitus/configs/etc/conf.d/snapper"
-mkdir -p /etc/conf.d/
-cp -rfv ${SNAPPER_CONF_D} /etc/conf.d/
+# SNAPPER_CONF_D="$HOME/ArchTitus/configs/etc/conf.d/snapper"
+# mkdir -p /etc/conf.d/
+# cp -rfv ${SNAPPER_CONF_D} /etc/conf.d/
 
-fi
+# fi
 
-echo -ne "
--------------------------------------------------------------------------
-               Enabling (and Theming) Plymouth Boot Splash
--------------------------------------------------------------------------
-"
-PLYMOUTH_THEMES_DIR="$HOME/ArchTitus/configs/usr/share/plymouth/themes"
-PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
-mkdir -p /usr/share/plymouth/themes
-echo 'Installing Plymouth theme...'
-cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
-if [[ $FS == "luks" ]]; then
-  sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
-  sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
-else
-  sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
-fi
-plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
-echo 'Plymouth theme installed'
+# echo -ne "
+# -------------------------------------------------------------------------
+#                Enabling (and Theming) Plymouth Boot Splash
+# -------------------------------------------------------------------------
+# "
+# PLYMOUTH_THEMES_DIR="$HOME/ArchTitus/configs/usr/share/plymouth/themes"
+# PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
+# mkdir -p /usr/share/plymouth/themes
+# echo 'Installing Plymouth theme...'
+# cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
+# if [[ $FS == "luks" ]]; then
+#   sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+#   sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
+# else
+#   sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+# fi
+# plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
+# echo 'Plymouth theme installed'
 
 echo -ne "
 -------------------------------------------------------------------------
