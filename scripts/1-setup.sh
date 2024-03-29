b2518a41fbcc829d8f101ea003390566a8e8adb5
Rust1667 <topphotoblog_byddwn@aleeas.com>
2024-03-29 01:49:24 +0100

    more minimal

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -112,17 +112,18 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 # Graphics Drivers find and install
-gpu_type=$(lspci)
-if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-    pacman -S --noconfirm --needed nvidia
-	nvidia-xconfig
-elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
-    pacman -S --noconfirm --needed xf86-video-amdgpu
-elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
-    pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
-elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
-    pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
-fi
+# gpu_type=$(lspci)
+# if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+#     pacman -S --noconfirm --needed nvidia
+# 	nvidia-xconfig
+# elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+#     pacman -S --noconfirm --needed xf86-video-amdgpu
+# elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
+#     pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+# elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
+#     pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+# fi
+pacman -S --noconfirm --needed libva-intel-driver
 #SETUP IS WRONG THIS IS RUN
 if ! source $HOME/ArchTitus/configs/setup.conf; then
 	# Loop through user input until the user gives a valid username
